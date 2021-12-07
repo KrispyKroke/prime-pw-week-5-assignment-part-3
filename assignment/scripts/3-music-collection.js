02479b8d91cf48f1d1ea1e240798a804ce65e270
Jared Kroke <jared.kroke@gmail.com>
2021-12-07 11:04:25 -0600

    Tested the addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,3 +11,12 @@ function addToCollection(title, artist, yearPublished) {
   collection.push(albumObject);
   return albumObject;
 }
+
+console.log(addToCollection('Currents', "Tame Impala", 2015));
+console.log(addToCollection('Lonerism', "Tame Impala", 2012));
+console.log(addToCollection('Runner', 'Tennis', 2020));
+console.log(addToCollection("The Slow Rush", "Tame Impala", 2020));
+console.log(addToCollection("King Krule", "King Krule", 2011));
+console.log(addToCollection("Death of a Party Girl", "TV Girl", 2018));
+
+console.log(collection);
