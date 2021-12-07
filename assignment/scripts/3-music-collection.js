ee72fefb89f9a348ee79c210819958ff7e60cef8
Jared Kroke <jared.kroke@gmail.com>
2021-12-07 10:52:53 -0600

    Created addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,13 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let albumObject = {
+    'title': title,
+    'artist': artist,
+    "year published": yearPublished
+  };
+  collection.push(albumObject);
+  return albumObject;
+}
